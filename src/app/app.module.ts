016a9b96ae7ed8d4244a16745be61f4794a72e83
Luis Romero Pretel <marco2002rp@gmail.com>
2024-11-15 18:30:12 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+
+import localeEsHN from '@angular/common/locales/es-HN';
+import localeFrCA from '@angular/common/locales/fr-CA';
+
+import { registerLocaleData } from '@angular/common';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+
+registerLocaleData( localeEsHN );
+registerLocaleData( localeFrCA );
 
 @NgModule({
   declarations: [
@@ -10,9 +21,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es-HN' },
+    provideAnimations()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
